48ea3d375d7b0d330a47005c195fa55f9dc3bd05
Jérémy <jerem.blcn@gmail.com>
2020-02-13 21:09:02 +0100

    Recherche ok

---
+++ API/TMDBApi.js
@@ -0,0 +1,8 @@
+const API_TOKEN = "ed9bbe0cba2fbdc79b2381356d081807";
+
+export function getFilmsFromApiWithSearchedText (text) {
+    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text
+    return fetch(url)
+        .then((response) => response.json())
+        .catch((error) => console.error(error))
+}
--- Components/Search.js
+++ Components/Search.js
@@ -1,26 +1,65 @@
 // Components/Search.js
 import React from 'react'
-import { StyleSheet, View, TextInput, Button } from 'react-native'
+import { StyleSheet, View, TextInput, Button, FlatList, Text } from 'react-native'
+import FilmItem from "./FilmItem"
+import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi'
 
 class Search extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.searchedText = ""
+        this.state = {
+            films: [] }
+    }
+
+    _loadFilms() {
+        if (this.searchedText.length > 0) { // Seulement si le texte recherché n'est pas vide
+            getFilmsFromApiWithSearchedText(this.searchedText).then(data => {
+                this.setState({ films: data.results })
+            })
+        }
+    }
+
+    _searchTextInputChanged(text) {
+        this.searchedText = text // Modification du texte recherché à chaque saisie de texte, sans passer par le setState comme avant
+    }
+
     render() {
         return (
-            <View style={styles.container}>
-                <TextInput placeholder='Titre du film'/>
-                <Button title='Rechercher' onPress={() => {}}/>
+            <View style={styles.main_container}>
+                <TextInput
+                    style={styles.textinput}
+                    placeholder='Titre du film'
+                    onChangeText={(text) => this._searchTextInputChanged(text)}
+                />
+                <Button title='Rechercher' onPress={() => this._loadFilms()}/>
+                <FlatList
+                    data={this.state.films}
+                    keyExtractor={(item) => item.id.toString()}
+                    renderItem={({item}) => <FilmItem film={item}/>}
+                />
             </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
+    main_container: {
         flex: 1,
-        backgroundColor: 'yellow',
-        alignItems: 'center',
-        justifyContent: 'center',
+        marginTop: 40
     },
-});
+    textinput: {
+        marginLeft: 5,
+        marginRight: 5,
+        height: 50,
+        borderColor: 'green',
+        borderWidth: 2,
+        borderRadius: 5,
+        paddingLeft: 5,
+        marginBottom: 10
+    }
+})
 
 export default Search
 
